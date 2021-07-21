ba1d03639708ff8081bfb2bb95c790f3e1388418
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2021-07-21 15:21:25 +0200

    Add Category entity and reposotory.

--- src/main/java/pl/coderslab/charity/domain/model/Category.java
+++ src/main/java/pl/coderslab/charity/domain/model/Category.java
@@ -1,2 +1,24 @@
-package pl.coderslab.charity.domain.model;public class Category {
+package pl.coderslab.charity.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import pl.coderslab.charity.utils.SQLConstants;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = SQLConstants.CATEGORY)
+@Getter
+@Setter
+@ToString
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column @NotEmpty
+    private String name;
+
 }
