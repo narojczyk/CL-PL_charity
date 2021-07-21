f6d67cdad452b89965b3455af5b8abb727f3a42e
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2021-07-21 15:20:48 +0200

    Add utility class for SQL-related name constants.

---
+++ src/main/java/pl/coderslab/charity/domain/model/Category.java
@@ -0,0 +1,2 @@
+package pl.coderslab.charity.domain.model;public class Category {
+}
---
+++ src/main/java/pl/coderslab/charity/utils/SQLConstants.java
@@ -0,0 +1,5 @@
+package pl.coderslab.charity.utils;
+
+public class SQLConstants {
+    public static final String CATEGORY = "categories";
+}
