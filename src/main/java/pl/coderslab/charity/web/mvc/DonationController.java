20a534136079486d6f92ea08a6cdcc1dbf72d76d
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2021-07-25 21:36:02 +0200

    Add donation controller.

---
+++ src/main/java/pl/coderslab/charity/web/mvc/DonationController.java
@@ -0,0 +1,9 @@
+package pl.coderslab.charity.web.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/donations")
+public class DonationController {
+}
